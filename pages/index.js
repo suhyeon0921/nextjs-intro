2c78bcaf36f3c88169c1f8f2a66c272a608ef7fc
suhyeon <suhyeon3484@naver.com>
2023-03-17 23:27:44 +0900

    #2.2 Redirect and Rewrite

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,45 @@
 import Seo from '@component/components/Seo';
 import { useEffect, useState } from 'react';
 
-const API_KEY = 'f1238be62c41593541fe8a48c195cf64';
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
   return (
-    <div>
+    <div className='container'>
       <Seo title='Home' />
       {!movies && <h4>Loading...</h4>}
-      {/* ?넣어서 movies가 없으면 실행되지 않도록 함 */}
       {movies?.map((movie) => (
-        <>
-          <div key={movie.id}></div>
+        <div className='movie' key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
-        </>
+        </div>
       ))}
-      <h1>hello</h1>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
